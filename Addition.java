8baf6905f489e50e6f79727f3918c646c359a787
MoluguBhargavisharma <102900168+MoluguBhargavisharma@users.noreply.github.com>
2024-01-18 13:30:14 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,13 +1,10 @@
-package com.orchard;
+
 
 public class Addition {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
-		int c;
-		c=a+b;
-		System.out.println("c value is"+c);
+		
+		System.out.println("BHARGAVI");
 
 	}
 
